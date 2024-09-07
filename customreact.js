437c97d36c832af962487c1c6e964bec7eb3a52a
Ravi Kumar <you@example.com>
2024-09-08 00:52:26 +0530

    code of creating tag includede

--- customreact.js
+++ customreact.js
@@ -1,7 +1,27 @@
 
 const box=document.getElementById("box");
 
-const para=document.createElement('p');
+const data={
+    type:'a',
+    props:{
+        href:"https://google.com",
+        target:'_blank'
+    },
+    children:'click me to visit google'
+}
 
-para.innerHTML=`<h1>hello</h1>`;
-box.appendChild(para);
+function customrender(data,box){
+    const domElement=document.createElement(data.type);
+    domElement.innerHTML=`${data.children}`;
+    domElement.setAttribute('href',data.props.href);
+    domElement.setAttribute('target',data.props.target);
+     box.appendChild(domElement);
+
+}
+
+customrender(data,box);
+
+// const para=document.createElement('p');
+
+// para.innerHTML=`<h1>hello</h1>`;
+// box.appendChild(para);
